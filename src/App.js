413a7e12a6fcce76de10380b3e1a29e8ee0d7844
Jerzy <118739912+jszufa@users.noreply.github.com>
2023-05-11 13:37:34 +0200

    wersja dla statycznej bazy pojedynków - do dopracowania

--- src/App.js
+++ src/App.js
@@ -6,10 +6,27 @@ let maleNamesArray = ["Ignacy", "Bernard", "Hugon", "Antoni", "Tadeusz", "Jan", 
 let initialWeightsArray = maleNamesArray.map(name => [name, 0]);
 
 
+const createStaticCompareblePairs = (array) => {
+
+  let staticCompareArray = [];
+
+  for (let x = 0; x < array.length; x++) {
+    for (let n = 1; n < array.length - x; n++) {
+      staticCompareArray.push([array[x], array[x + n]]);
+    }
+  }
+
+  return staticCompareArray;
+}
+
+let newCompareArray = createStaticCompareblePairs(maleNamesArray);
+console.log(newCompareArray);
+
+
 function App() {
   return (
     <div className="App">
-      <NamesComparer maleNamesArray={maleNamesArray} initialWeightsArray={initialWeightsArray}/>
+      <NamesComparer maleNamesArray={maleNamesArray} initialWeightsArray={initialWeightsArray} compareArray={newCompareArray}/>
     </div>
   );
 }
--- src/NamesComparer.js
+++ src/NamesComparer.js
@@ -3,42 +3,44 @@ import './NamesComparer.css';
 
 function NamesComparer(props) {
 
-  
-
   const [weightsArray, setWeightsArray] = useState(props.initialWeightsArray);
-  const [currentNames, setCurrentNames] = useState([props.maleNamesArray[0], props.maleNamesArray[1]]);
+  const [currentNames, setCurrentNames] = useState(props.compareArray[0]);
 
   const updateCurrentNames = (winnerName) => {
     console.log(winnerName)
 
     //teraz żeby aktualizować wagi, biorę pod uwagę każdy element tablicy. Może jest szybszy sposób? Np. z filtrowaniem?
     setWeightsArray(
-      weightsArray.map( name => (name[0] === winnerName) ? [name[0], ++name[1]] : name )
+      weightsArray.map(name => (name[0] === winnerName) ? [name[0], ++name[1]] : name)
     )
 
     //wagi dobrze się aktualizują
-    console.log (weightsArray);
-
-    //powinienem wybierać imiona, póki co tylko losuję, więc mogą być powtórki
-    const random1 = Math.floor(Math.random() * props.maleNamesArray.length);
-    const random2 = Math.floor(Math.random() * props.maleNamesArray.length);
+    console.log(weightsArray);    
     
-    setCurrentNames([props.maleNamesArray[random1], props.maleNamesArray[random2]])
+  //wariant pojedynku każdy z każdym
+
+    //na razie konkretrna liczba - "36" tylko roboczo
+    if (/* warunek odpowiedni */) {
+    setCurrentNames((lastPair) =>
+    props.compareArray[props.compareArray.indexOf(lastPair) + 1]);
+    }
+    else {
+      setCurrentNames(['Koniec', 'Pojedynku'])
+      console.log(weightsArray);
+    }
 
   }
 
+  /*  const random = Math.floor(Math.random() * maleNamesArray.length); */
 
-
- /*  const random = Math.floor(Math.random() * maleNamesArray.length); */
-
-  const updateNameWeights = (/* oba aktualne imiona */) => {
+  
 
     //Wygrany = waga+1 + aktualna(albo raczej stała Waga pokonanego
     //jeśli zakładam przechodniość przekonań, to inne pojedynki przegranego, powinny wpływać na wynik wygranego
     // Przegrany = nic
     //wyświetl nowe imiona
 
-  }
+
 
   return (
     <div className="NamesComparer">
