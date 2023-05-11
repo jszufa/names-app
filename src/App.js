5690589f6fbd71cbed00b4eb124bb43535ce13dd
Jerzy <118739912+jszufa@users.noreply.github.com>
2023-05-11 12:24:51 +0200

    initial work

--- src/App.js
+++ src/App.js
@@ -2,10 +2,14 @@ import './App.css';
 
 import NamesComparer from './NamesComparer';
 
+let maleNamesArray = ["Ignacy", "Bernard", "Hugon", "Antoni", "Tadeusz", "Jan", "Stanisław", "Łukasz", "Marcin"];
+let initialWeightsArray = maleNamesArray.map(name => [name, 0]);
+
+
 function App() {
   return (
     <div className="App">
-      <NamesComparer />
+      <NamesComparer maleNamesArray={maleNamesArray} initialWeightsArray={initialWeightsArray}/>
     </div>
   );
 }
--- src/NamesComparer.js
+++ src/NamesComparer.js
@@ -1,32 +1,50 @@
 import { useState } from 'react';
 import './NamesComparer.css';
 
-function NamesComparer() {
-  
-  const[weightsArray, setWeightsArray] = useState([])
-  
-  let maleNamesArray = ["Ignacy", "Bernard", "Hugon", "Antoni", "Tadeusz", "Jan", "Stanisław", "Łukasz", "Marcin"];
+function NamesComparer(props) {
 
-  //adding initial weights
-  let initialWeightsArray = maleNamesArray.map(name => [name, 0]);
-  //console.log(maleNamesArray.map(name => [name, 0]))
+  
 
-  const random = Math.floor(Math.random() * maleNamesArray.length);
+  const [weightsArray, setWeightsArray] = useState(props.initialWeightsArray);
+  const [currentNames, setCurrentNames] = useState([props.maleNamesArray[0], props.maleNamesArray[1]]);
+
+  const updateCurrentNames = (winnerName) => {
+    console.log(winnerName)
+
+    //teraz żeby aktualizować wagi, biorę pod uwagę każdy element tablicy. Może jest szybszy sposób? Np. z filtrowaniem?
+    setWeightsArray(
+      weightsArray.map( name => (name[0] === winnerName) ? [name[0], ++name[1]] : name )
+    )
+
+    //wagi dobrze się aktualizują
+    console.log (weightsArray);
+
+    //powinienem wybierać imiona, póki co tylko losuję, więc mogą być powtórki
+    const random1 = Math.floor(Math.random() * props.maleNamesArray.length);
+    const random2 = Math.floor(Math.random() * props.maleNamesArray.length);
+    
+    setCurrentNames([props.maleNamesArray[random1], props.maleNamesArray[random2]])
+
+  }
+
+
+
+ /*  const random = Math.floor(Math.random() * maleNamesArray.length); */
 
   const updateNameWeights = (/* oba aktualne imiona */) => {
-      
+
     //Wygrany = waga+1 + aktualna(albo raczej stała Waga pokonanego
     //jeśli zakładam przechodniość przekonań, to inne pojedynki przegranego, powinny wpływać na wynik wygranego
     // Przegrany = nic
     //wyświetl nowe imiona
 
   }
-  
+
   return (
     <div className="NamesComparer">
       {/* {initialWeightsArray} */}
-      <button className='NameButton' onClick>{maleNamesArray[random]}</button>
-      <button className='NameButton' onClick>Bartek</button>
+      <button className='NameButton' onClick={() => updateCurrentNames(currentNames[0])}>{currentNames[0]}</button>
+      <button className='NameButton' onClick={() => updateCurrentNames(currentNames[1])}>{currentNames[1]}</button>
     </div>
   );
 }
