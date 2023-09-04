c70f319ef9d9ffa831e8c240e67b0879b8a77f5a
Jerzy <118739912+jszufa@users.noreply.github.com>
2023-09-04 17:37:03 +0200

    styles

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import NamesComparerMS from './NamesComparerMS';
 import Names from './Names';
 import Results from './Results';
 import babyboy from './img/baby-boy.png';
+import create from './img/create.png';
 
 function App() {
 
@@ -27,20 +28,18 @@ function App() {
     <div className='App'>
       <header className='Header'>
         <h1>Choose together</h1>
-        <p>Choose a name for the baby without a fight</p>
+        <p>Choose a name for your baby without a fight</p>
         <img className='Baby' src={babyboy}/>
       </header>
       <section className='Names-pool'>
+        <img src={create} className='CreateIcon'/>
         <h2>1. Create pool of names</h2>
         <p>Together with your partner, create a list of names from which you want to choose. Each of you can enter your name suggestions here. To remove a name from the pool just click on it.</p>
         <Names names={namesPool} setNames={setNamesPool} />
-      </section>
-      <section className='Dealbreakers'>
-        <h2>2. Cross out dealbreakers</h2>
-        <p>Great! Your pool of names is almost ready. Now each of you can look at it again and cross out few names (can be zero) you are 100% sure you won't name your child. </p>
+        <p>Cross out dealbreakers. Great! Your pool of names is almost ready. Now each of you can look at it again and cross out few names (can be zero) you are 100% sure you won't name your child. </p>
       </section>
       <section className='Comparison'>
-        <h2>3. Duels of names</h2>
+        <h2>2. Duels of names</h2>
         <p> Complete a series of name compraisons to determine your preference ranking. In this part of the proces you will individually choose which name from the dispalyed pair you'd prefer to give to your child. </p>
         <p>First, person A performs a series of comparisons in the window below. Then person B performs their own series of comparisons in the second window.</p>
 
@@ -56,11 +55,12 @@ function App() {
         </div>
       </section>
       <section className='ResultsSection'>
-        <h2>4. See your top names</h2>
+        <h2>3. See your top names</h2>
         <p> The lower the score the better!</p>
         <p> If, for example, "Catlyn" got 3 points, it means that the name was in 3rd place for the person who likes the name less. In this type of decision there is usually one side that is less satisfied. This ranking maximizes the satisfaction of this person. </p>
         <Results ranking={ranking} />
       </section>
+      <footer className='Footer'>All rights reserved</footer>
     </div>
   );
 }
