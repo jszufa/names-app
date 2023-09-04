0112a7b5f13f08c9ed841e8875bf33ecb587783e
Jerzy <118739912+jszufa@users.noreply.github.com>
2023-09-04 17:06:07 +0200

    header styling

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import { useEffect, useState } from 'react';
 import NamesComparerMS from './NamesComparerMS';
 import Names from './Names';
 import Results from './Results';
+import babyboy from './img/baby-boy.png';
 
 function App() {
 
@@ -24,6 +25,11 @@ function App() {
 
   return (
     <div className='App'>
+      <header className='Header'>
+        <h1>Choose together</h1>
+        <p>Choose a name for the baby without a fight</p>
+        <img className='Baby' src={babyboy}/>
+      </header>
       <section className='Names-pool'>
         <h2>1. Create pool of names</h2>
         <p>Together with your partner, create a list of names from which you want to choose. Each of you can enter your name suggestions here. To remove a name from the pool just click on it.</p>
